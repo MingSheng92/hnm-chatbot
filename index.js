e6da27cbeb92ab76727cdcb121cbe990a0420978
MingSheng92 <58675240+MingSheng92@users.noreply.github.com>
2021-03-19 22:10:08 +0800

    update 0.21

--- index.js
+++ index.js
@@ -112,13 +112,50 @@ app.get('/webhook', (req, res) => {
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
   let response;
-  //console.log('handleMEssage message:', JSON.stringify(received_message));
 
-  //const locationAttachment = received_message && received_message.attachments && received_message.attachments.find(a => a.type === 'location');
-  //const coordinates = locationAttachment && locationAttachment.payload && locationAttachment.payload.coordinates;
+  // Checks if the message contains text
+  if (received_message.text) {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    //let attachment_url = received_message.attachments[0].payload.url;
+    console.log(received_message.attachments[0])
 
-  //console.log(locationAttachment)
-  //console.log(coordinates)
+    /*
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "YES",
+                "payload": ACKNOWLEDGE_YES,
+              },
+              {
+                "type": "postback",
+                "title": "NO",
+                "payload": ACKNOWLEDGE_NO,
+              }
+            ],
+          }]
+        }
+      }
+    }
+    */
+  } 
+    
+  // Send the response message
+  callSendAPI(sender_psid, response);    
 }
 
 function handlePostback(sender_psid, received_postback) {
