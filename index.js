eeffd6be4f894783089a9fdb89f4b41b95792ba7
MingSheng92 <58675240+MingSheng92@users.noreply.github.com>
2021-03-19 15:57:25 +0800

    update 0.1g
    remove handleMessages for now

--- index.js
+++ index.js
@@ -107,47 +107,7 @@ app.get('/webhook', (req, res) => {
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
     let response;
-    console.log("Handling normal message event: ")
-    
-    // Checks if the message contains text
-    if (received_message.text) {    
-      // Create the payload for a basic text message, which
-      // will be added to the body of our request to the Send API
-      response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-      }
-    } else if (received_message.attachments) {
-      // Get the URL of the message attachment
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "generic",
-            "elements": [{
-              "title": "Is this the right picture?",
-              "subtitle": "Tap a button to answer.",
-              "image_url": attachment_url,
-              "buttons": [
-                {
-                  "type": "postback",
-                  "title": "YES",
-                  "payload": ACKNOWLEDGE_YES,
-                },
-                {
-                  "type": "postback",
-                  "title": "NO",
-                  "payload": ACKNOWLEDGE_NO,
-                }
-              ],
-            }]
-          }
-        }
-      }
-    } 
-    
-    // Send the response message
-    callSendAPI(sender_psid, response);    
+    console.log('handleMEssage message:', JSON.stringify(message));
 }
 
 function handlePostback(sender_psid, received_postback) {
